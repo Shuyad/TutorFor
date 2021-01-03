77209e162c18d6dec6411c65e80494763ad178b3
Saransh Hardaha <hellosaranshh@gmail.com>
2021-01-03 17:51:51 +0530

    Linked Pages!

--- Main/urls.py
+++ Main/urls.py
@@ -4,5 +4,10 @@ from Main import views
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('about',views.about,name='about')
+    path('about',views.about,name='about'),
+    path('support',views.support,name='support'),
+    path('user/login',views.login,name='login'),
+    path('user/signup',views.signup,name='signup'),
+    path('user/forgotPassword',views.forgotPassword,name='forgotPassword'),
+    path('user/dashboard',views.dashboard,name='dashboard'),
 ]
--- Main/views.py
+++ Main/views.py
@@ -6,3 +6,22 @@ def index(request):
 
 def about(request):
     return render(request,'about.html')
+
+def support(request):
+    return render(request,'support.html')
+
+def login(request):
+    return render(request,'user/login.html')
+
+def signup(request):
+    return render(request,'user/signup.html')
+
+def forgotPassword(request):
+    return render(request,'user/forgotPassword.html')
+
+def dashboard(request):
+    return render(request,'user/dashboard/index.html')
+
+# def ChangePassword(request):
+#     return render(request,'user/dashboard/changePassword.html')
+
